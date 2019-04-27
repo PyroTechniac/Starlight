166d003ddb73d54a58cfb8fe0a0114e478725309
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-04-27 12:09:19 -0400

    fixed linting and blacklist command

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import { BaseCluster } from 'kurasuta';
-import StarlightClient from './client/StarlightClient';
-import { Client } from 'discord.js';
 
 export default class StarlightCluster extends BaseCluster {
     public async launch() {
