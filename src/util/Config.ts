7b9524d55a86cbbf3e9f051d6788a5f76c5fb37c
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-04-28 21:54:48 -0400

    added getters and readonly

--- src/util/Config.ts
+++ src/util/Config.ts
@@ -2,12 +2,20 @@ import StarlightClient from '../client/StarlightClient';
 import { DefaultConfigOptions } from './Constants';
 
 export class Config {
-    public token: string;
-    public prefix: string;
-    public constructor(public client: StarlightClient, options?: ConfigOptions) {
+    private _token: string;
+    private _prefix: string;
+    public constructor(public readonly client: StarlightClient, options?: ConfigOptions) {
         options = this.client.util.mergeDefault(DefaultConfigOptions, options);
-        this.token = options.token;
-        this.prefix = options.prefix;
+        this._token = options.token;
+        this._prefix = options.prefix;
+    }
+
+    public get token(): string {
+        return this._token;
+    }
+
+    public get prefix(): string {
+        return this._prefix;
     }
 }
 
