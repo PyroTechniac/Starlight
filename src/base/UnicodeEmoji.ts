43c2d1dbf8ce7f8f5c6244949288b05991282ab9
Owen Bellish <shadowguy224@live.com>
2019-04-29 14:26:42 -0400

    more stuff

--- src/base/UnicodeEmoji.ts
+++ src/base/UnicodeEmoji.ts
@@ -1,4 +1,13 @@
-export class UnicodeEmoji {}
+import { Client } from 'discord.js';
+
+export class UnicodeEmoji {
+    public name: string;
+    public icon: string;
+    public constructor(public client: Client, data: UnicodeEmojiData) {
+        this.name = data.name;
+        this.icon = data.icon;
+    }
+}
 
 export interface UnicodeEmojiData {
     name: string;
--- src/stores/UnicodeEmojiStore.ts
+++ src/stores/UnicodeEmojiStore.ts
@@ -1,8 +1,14 @@
-import { DataStore } from 'discord.js';
-import { UnicodeEmoji } from '../base/UnicodeEmoji';
+import { Client, DataStore } from 'discord.js';
+import { UnicodeEmoji, UnicodeEmojiData } from '../base/UnicodeEmoji';
 
 type UnicodeEmojiResolvable = string;
 
 export class UnicodeEmojiStore extends DataStore<string, UnicodeEmoji, typeof UnicodeEmoji, UnicodeEmojiResolvable> {
-
+    public constructor(client: Client) {
+        super(client, null, UnicodeEmoji);
+	}
+	
+	public init(data: UnicodeEmojiData[]): void {
+		for (const d of data) this.set(d.name, new UnicodeEmoji(this.client, d))
+	}
 }
