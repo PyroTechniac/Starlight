caf09d2d96d7288e4eed78c2f3189d156c043fe2
Owen Bellish <shadowguy224@live.com>
2019-05-07 08:43:27 -0400

    testing gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,22 @@
 /* eslint-disable */
 
 const gulp = require('gulp');
-const ts = require('gulp-typescript')
+const ts = require('gulp-typescript');
+const del = require('del');
+const out = 'dist/'
+
+const project = ts.createProject('tsconfig.json');
+
+const build = () => {
+	del.sync([`${out}/**/*.*`])
+	const tsCompile = gulp.src('src/**/*.ts')
+		.pipe(project())
+
+	tsCompile.pipe(gulp.dest(out))
+
+	gulp.src('src/**/*.js').pipe(gulp.dest(out))
+	gulp.src('src/**/*.json').pipe(gulp.dest(out))
+	return Promise.resolve();
+}
+
+exports.build = build;
