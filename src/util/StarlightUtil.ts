a5106fec617e537850e2a6236029a55e410b6700
Owen Bellish <shadowguy224@live.com>
2019-05-08 10:40:22 -0400

    more util

--- src/util/StarlightUtil.ts
+++ src/util/StarlightUtil.ts
@@ -6,6 +6,39 @@ import { Stream } from 'stream';
 export class ClientUtil {
     public constructor(public readonly client: Klasa.KlasaClient) { }
 
+    public resolveEmoji(text: string, emojis: Discord.Collection<Discord.Snowflake, Discord.Emoji>, caseSensitive = false, wholeWord = false): Discord.Emoji {
+        return emojis.get(text) || emojis.find((emoji): boolean => this.checkEmoji)
+    }
+
+    public resolveEmojis(text: string, emojis: Discord.Collection<Discord.Snowflake, Discord.Emoji>, caseSensitive = false, wholeWord = false): Discord.Collection<Discord.Snowflake, Discord.Emoji> {
+        return emojis.filter((emoji): boolean => this.checkEmoji(text, emoji, caseSensitive, wholeWord));
+    }
+
+    public checkEmoji(text: string, emoji: Discord.Emoji, caseSensitive = false, wholeWord = false): boolean {
+        if (emoji.id === text) return true;
+
+        const reg = /<a?:[a-zA-Z0-9_]+:(\d{17,19})>/
+        const match = text.match(reg);
+
+        if (match && emoji.id === match[1]) return true;
+
+        text = caseSensitive ? text : text.toLowerCase();
+        const name = caseSensitive ? emoji.name : emoji.name.toLowerCase();
+
+        if (!wholeWord) {
+            return name.includes(text) || name.includes(text.replace(/:/, ''));
+        }
+        return name === text || name === text.replace(/:/, '');
+    }
+
+    public resolveGuild(text: string, guilds: Discord.Collection<Discord.Snowflake, Klasa.KlasaGuild>, caseSensitive = false, wholeWord = false): Klasa.KlasaGuild | undefined {
+        return guilds.get(text) || guilds.find((guild): boolean => this.checkGuild(text, guild, caseSensitive, wholeWord));
+    }
+
+    public resolveGuilds(text: string, guilds: Discord.Collection<Discord.Snowflake, Klasa.KlasaGuild>, caseSensitive = false, wholeWord = false): Discord.Collection<Discord.Snowflake, Klasa.KlasaGuild> {
+        return guilds.filter((guild): boolean => this.checkGuild(text, guild, caseSensitive, wholeWord));
+    }
+
     public checkGuild(text: string, guild: Klasa.KlasaGuild, caseSensitive = false, wholeWord = false): boolean {
         if (guild.id === text) return true;
         text = caseSensitive ? text : text.toLowerCase();
