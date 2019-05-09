c111a5585f74ea71305504a5014bc257bd9b0971
Owen Bellish <shadowguy224@live.com>
2019-05-09 08:50:15 -0400

    fixed some linting

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-import { StarlightClient } from './client/Client';
 import { config } from 'dotenv';
+import { SchemaFolder } from 'klasa';
+import { StarlightClient } from './client/Client';
 config();
 StarlightClient.use(require('./plugins/functions'))
     .use(require('@kcp/channels-gateway'));
 
 StarlightClient.defaultPermissionLevels
-    .add(4, ({ guild, member }) => guild! && member!.permissions.has('KICK_MEMBERS'), { fetch: true })
-    .add(5, ({ guild, member }) => guild! && member!.permissions.has('BAN_MEMBERS'), { fetch: true })
+    .add(4, ({ guild, member }): boolean => guild! && member!.permissions.has('KICK_MEMBERS'), { fetch: true })
+    .add(5, ({ guild, member }): boolean => guild! && member!.permissions.has('BAN_MEMBERS'), { fetch: true });
 
 StarlightClient.defaultGuildSchema
     .add('antiinvite', 'boolean', { default: false })
-    .add('channels', folder => folder
+    .add('channels', (folder): SchemaFolder => folder
         .add('modlog', 'TextChannel'))
-    .add('modlogs', 'any', { array: true })
+    .add('modlogs', 'any', { array: true });
 
 const production = process.env.NODE_ENV === 'production';
 
