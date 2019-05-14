f39b8a3fca778eeb908a86911ff14fa4dea44202
Owen Bellish <shadowguy224@live.com>
2019-05-14 14:08:27 -0400

    more arguments

---
+++ src/arguments/finalizers.ts
@@ -0,0 +1,11 @@
+import { Argument, ArgumentStore, KlasaClient, MultiArgument } from 'klasa';
+
+export default class MultiFinalizerArgument extends MultiArgument {
+	public constructor(client: KlasaClient, store: ArgumentStore, file: string[], directory: string) {
+		super(client, store, file, directory, {aliases: ['...finalizer']})
+	}
+
+	public get base(): Argument {
+		return this.store.get('finalizer');
+	}
+}
