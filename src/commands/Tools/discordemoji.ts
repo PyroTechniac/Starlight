6d8704b62f3e23ba9f51f20e0223f03d1bf1a457
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-06-05 23:19:09 -0400

    new command

---
+++ src/commands/Tools/discordemoji.ts
@@ -0,0 +1,47 @@
+import { Command, CommandStore, KlasaMessage } from 'klasa';
+import fetch from 'node-fetch';
+import { Message, TextChannel } from 'discord.js';
+
+const API_URL = 'https://discordemoji.com/api/';
+
+export default class extends Command {
+    private emojis: any;
+    public constructor(store: CommandStore, file: string[], directory: string) {
+        super(store, file, directory, {
+            aliases: ['de'],
+            description: 'Searches discordemoji.com for an emoji.',
+            usage: '<query:str{1,20}> [count:int{1,100}]',
+            usageDelim: ',',
+            runIn: ['text']
+        });
+    }
+
+    public async run(msg: KlasaMessage, [query, count = 4]: [string, number?]): Promise<KlasaMessage | KlasaMessage[]> {
+        const matches = this.emojis.filter(({ nsfw, title }): any => {
+            if (!(msg.channel as TextChannel).nsfw && nsfw) return false;
+            return title.toUpperCase().includes(query.toUpperCase());
+        });
+
+        if (matches.length === 0) throw 'No results found.';
+        return msg.send(
+            matches
+                .sort((): number => Math.random() * 0.5)
+                .slice(0, count)
+                .map((emj): any => emj.image)
+                .join(' ')
+        );
+    }
+
+    public async init(): Promise<void> {
+        const [emojis, cats] = await Promise.all(
+            [API_URL, `${API_URL}?request=categories`].map((url): any => fetch(url).then((res): any => res.json()))
+        );
+
+        this.emojis = emojis.map((emj): any => ({
+            ...emj,
+            category: cats[emj.category],
+            nsfw: cats[emj.category] === 'NSFW',
+            description: emj.description.includes('View more') ? '' : emj.description
+        }));
+    }
+}
