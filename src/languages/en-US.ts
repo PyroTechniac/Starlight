3f43acd91fc08fe4759d9b74a5ea71d08dde3221
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-06-18 21:41:00 -0400

    prefix command

--- src/languages/en-US.ts
+++ src/languages/en-US.ts
@@ -5,7 +5,10 @@ export default class extends Language {
         super(store, file, directory);
 
         this.language = {
-            COMMAND_SET_DESCRIPTION: 'Used to manage a guild. Sets a name of whatever subcommand is used.'
+            COMMAND_PREFIX_DESCRIPTION: 'Change the command prefix the bot uses in your server.',
+            COMMAND_PREFIX_RESET: `Switched back the guild's prefix to ${this.client.options.prefix!}`,
+            COMMAND_PREFIX_REMINDER: (prefix: string): string => `The prefix for this guild is \`${prefix}\``,
+            COMMAND_PREFIX_CHANGE: (prefix: string): string => `The prefix for this guild has been set to \`${prefix}\``
         };
     }
 }
