266c275863931b92598d82a4c77591d18df9ec2b
Owen Bellish <shadowguy224@live.com>
2019-08-27 08:44:23 -0400

    more work

--- src/events/onceReady.ts
+++ src/events/onceReady.ts
@@ -1,10 +1,28 @@
-import {Event, EventOptions} from 'klasa'
+import { Event, EventOptions, util } from 'klasa'
+import { Team } from 'discord.js'
 import { ApplyOptions } from '../lib';
+let retries = 0;
 
 @ApplyOptions<EventOptions>({
     event: 'ready',
     once: true
 })
 export default class extends Event {
-    
+    public async run(): Promise<void> {
+        try {
+            await Promise.all([
+                this.client.fetchVoiceRegions(),
+                this.client.fetchApplication()
+            ]);
+        } catch (err) {
+            if (++retries === 3) return process.exit();
+            this.client.emit('warning', `Unable to fetchVoiceRegions/fetchApplication at this time, waiting 5 seconds and retrying. Retries left: ${retries - 3}`);
+            await util.sleep(5000);
+            return this.run();
+        }
+    }
+
+    private resolveOwners(): string[] {
+        
+    }
 }
--- src/lib/StarlightClient.ts
+++ src/lib/StarlightClient.ts
@@ -1,5 +1,21 @@
-import { Client } from 'klasa'
+import { Client, KlasaClientOptions } from 'klasa';
+import { Collection, VoiceRegion } from 'discord.js';
+
+declare module 'discord.js' {
+    interface Client {
+        regions: null | Collection<string, VoiceRegion>;
+    }
+}
 
 export class StarlightClient extends Client { 
-    
+    public constructor(options: KlasaClientOptions = {}) {
+        super(options);
+
+        this.regions = null;
+    }
+
+    public async fetchVoiceRegions(): Promise<Collection<string, VoiceRegion>> {
+        this.regions = await super.fetchVoiceRegions();
+        return this.regions;
+    }
 }
