9bb50ff69fe877031cb02d70f9fd7f28cc416c9c
Owen Bellish <shadowguy224@live.com>
2019-09-09 13:43:31 -0400

    src: fix bug with member settings

--- src/lib/extensions/StarlightMember.ts
+++ src/lib/extensions/StarlightMember.ts
@@ -2,7 +2,7 @@ import { Structures, GuildMember } from 'discord.js';
 
 class StarlightMember extends Structures.get('GuildMember') {
 
-	public settings = this.client.gateways.get('members')!.acquire(this);
+	public settings = this.client.gateways.get('members')!.acquire(this, `${this.guild.id}.${this.id}`);
 
 	public toJSON(): object {
 		return { ...super.toJSON(), settings: this.settings.toJSON() };
