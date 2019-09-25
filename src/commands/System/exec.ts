5e944af56ba1ed1e5e79ad8389d64b011d63b4f1
Owen Bellish <shadowguy224@live.com>
2019-09-25 13:47:57 -0400

    src: add exec command

---
+++ src/commands/System/exec.ts
@@ -0,0 +1,27 @@
+import { Command, CommandOptions, util, KlasaMessage } from 'klasa';
+import { ApplyOptions } from '../../lib';
+const { codeBlock, exec } = util;
+
+@ApplyOptions<CommandOptions>({
+	aliases: ['execute'],
+	description: 'Execute commands in the terminal, use with EXTREME CAUTION', // TODO: Move this to the lang file
+	guarded: true,
+	permissionLevel: 10,
+	usage: '<expression:string>',
+	extendedHelp: 'Times out in 60 seconds by default. This can be changed with --timeout=TIME_IN_MILLISECONDS' // TODO: Also move this
+})
+export default class extends Command {
+
+	public async run(msg: KlasaMessage, [input]: [string]): Promise<KlasaMessage> {
+		await msg.send('Executing your command...');
+
+		const result = await exec(input, { timeout: 'timeout' in msg.flags ? Number(msg.flags.timeout) || 60000 : 60000 })
+			.catch((err): { stdout: null; stderr: string } => ({ stdout: null, stderr: err }));
+
+		const output = result.stdout ? `**\`OUTPUT\`**${codeBlock('prolog', result.stdout)}` : '';
+		const outerr = result.stderr ? `**\`ERROR\`**${codeBlock('prolog', result.stderr)}` : '';
+
+		return msg.sendMessage([output, outerr].join('\n') || 'Done. There was no output to stdout or stderr.');
+	}
+
+}
