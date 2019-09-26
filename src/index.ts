f2f4c42d221f8fd4efb1e7618126dff7f0c16de8
Owen Bellish <shadowguy224@live.com>
2019-09-26 14:20:07 -0400

    src: new permission level

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { StarlightClient } from './lib';
+import { StarlightClient, ClientSettings } from './lib';
 import { SchemaFolder, KlasaClientOptions } from 'klasa';
 import { ShardingManager } from 'kurasuta'
 import { join } from 'path'
@@ -15,24 +15,11 @@ StarlightClient
     .add('channels', (folder: SchemaFolder): SchemaFolder => folder
         .add('modlogs', 'TextChannel'));
 
-// new StarlightClient({
-// 	prefix,
-// 	consoleEvents: {
-// 		debug: true,
-// 		log: true,
-// 		error: true,
-// 		verbose: true,
-// 		warn: true
-// 	},
-// 	commandEditing: true,
-// 	commandLogging: true,
-// 	fetchAllMembers: true,
-// 	schedule: {
-// 		interval: 'INTERVAL' in process.env ? Number(process.env.INTERVAL) || 5000 : 5000
-// 	},
-// 	readyMessage: (client: StarlightClient): string => `[INTERNAL] Successfully initialized. Ready to serve ${client.guilds.size} guild${client.guilds.size === 1 ? '' : 's'}.`,
-// 	restRequestTimeout: 60000
-// }).login(token);
+StarlightClient
+    .defaultPermissionLevels
+    .add(10, ({ client, author }) => {
+        return client.owners.has(author!) || (client.settings!.get(ClientSettings.Owners) as ClientSettings.Owners).includes(author!.id);
+    })
 
 const sharder = new ShardingManager(join(__dirname, 'main'), {
     token,
