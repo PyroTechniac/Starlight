2d5de174c230372a1e8658b6c7cead882b71fa80
Gryffon Bellish <owenbellish@gmail.com>
2019-09-29 20:44:37 -0400

    fix(alias): fix module alias

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ addAliases({
 	'@settings': `${__dirname}/lib/settings`,
 	'@structures': `${__dirname}/lib/structures`,
 	'@utils': `${__dirname}/lib/util`,
-	'@typings': `${__dirname}/lib/types`
+	'@typings': `${__dirname}/lib/typings`
 });
 
 StarlightClient
