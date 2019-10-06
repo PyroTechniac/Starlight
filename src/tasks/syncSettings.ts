620eb69825b0b80351f9b89478568fbd7d4ef380
Gryffon Bellish <owenbellish@gmail.com>
2019-10-06 19:41:33 -0400

    src: force sync

--- src/tasks/syncSettings.ts
+++ src/tasks/syncSettings.ts
@@ -5,7 +5,7 @@ export default class extends Task {
 
 	public async run(): Promise<void> {
 		await Promise.all([
-			this.client.settings!.sync(),
+			this.client.settings!.sync(true),
 			this.members.map((member): Promise<Settings> => member.settings.sync(true)),
 			this.client.guilds.map((guild): Promise<Settings> => guild.settings.sync(true)),
 			this.client.users.map((user): Promise<Settings> => user.settings.sync(true))
