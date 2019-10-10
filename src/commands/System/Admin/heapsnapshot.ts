dd60744e5445a0386ab345e39a3b6f23baa7ac18
Gryffon Bellish <owenbellish@gmail.com>
2019-10-10 08:08:19 -0400

    deps: redo graphs

--- src/commands/System/Admin/heapsnapshot.ts
+++ src/commands/System/Admin/heapsnapshot.ts
@@ -1,24 +1,19 @@
-import { writeSnapshot as writeSnapshotSync } from 'heapdump';
 import { Command, CommandOptions, KlasaMessage, Language } from 'klasa';
-import { join } from 'path';
-import { promisify } from 'util';
 import { ApplyOptions } from '../../../lib/util/Decorators';
-
-const writeSnapshot = promisify(writeSnapshotSync) as (path: string) => Promise<void>;
+import { writeHeapSnapshot } from 'v8';
 
 @ApplyOptions<CommandOptions>({
 	permissionLevel: 10,
 	guarded: true,
 	description: (lang: Language): string => lang.get('COMMAND_HEAPSNAPSHOT_DESCRIPTION'),
-	extendedHelp: (lang: Language): string => lang.get('COMMAND_HEAPSNAPSHOT_EXTENDEDHELP'),
-	runIn: ['dm']
+	extendedHelp: (lang: Language): string => lang.get('COMMAND_HEAPSNAPSHOT_EXTENDEDHELP')
 })
 export default class extends Command {
 
 	public async run(msg: KlasaMessage): Promise<KlasaMessage> {
 		await msg.sendLocale('COMMAND_HEAPSNAPSHOT_CAPTURING', [(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)]);
-		const path = join(process.cwd(), `${Date.now()}.heapsnapshot`);
-		await writeSnapshot(path);
+
+		const path = writeHeapSnapshot();
 
 		return msg.sendLocale('COMMAND_HEAPSNAPSHOT_CAPTURED', [path]);
 	}
--- src/lib/settings/GuildSettings.ts
+++ src/lib/settings/GuildSettings.ts
@@ -1,11 +1,7 @@
+
 /* eslint-disable @typescript-eslint/no-namespace */
 
 export namespace GuildSettings {
 	export type Prefix = string;
 	export const Prefix = 'prefix';
-
-	export namespace Channels {
-		export type ModLog = string;
-		export const ModLog = 'channels.modlog';
-	}
 }
