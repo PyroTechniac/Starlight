4de4cb7425f67f272987957d1442e97f1c1bc10c
Gryffon Bellish <owenbellish@gmail.com>
2019-10-12 14:02:21 -0400

    code: add workspaces

--- native.d.ts
+++ native.d.ts
@@ -1,5 +1,3 @@
 // This is to declare all native functions written in rust, as TypeScript doesn't recognize .node files as exports
 
-export declare function hello(): string;
-
 export declare function version(): string;
--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,8 +1,8 @@
 #[macro_use]
 extern crate neon;
 
+use neon::meta::{BUILD_PROFILE, VERSION};
 use neon::prelude::*;
-use neon::meta::{VERSION, BUILD_PROFILE};
 
 fn version(mut cx: FunctionContext) -> JsResult<JsString> {
     let mut complete_version = VERSION.to_owned();
@@ -12,5 +12,6 @@ fn version(mut cx: FunctionContext) -> JsResult<JsString> {
 }
 
 register_module!(mut cx, {
-    cx.export_function("version", version)
+    cx.export_function("version", version)?;
+    Ok(())
 });
