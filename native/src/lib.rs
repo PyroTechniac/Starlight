7974787de9cd8c63994a127bb026cc9a8c311fae
Gryffon Bellish <owenbellish@gmail.com>
2019-10-15 20:47:19 -0400

    src: remove rust as using rust + js gives performance issues

--- native/src/lib.rs
+++
@@ -1,7 +0,0 @@
-use wasm_bindgen::prelude::*;
-
-// This creates a JS function that returns `8` and is named returnEight
-#[wasm_bindgen(js_name = returnEight)]
-pub fn return_eight() -> u8 {
-    8
-}
