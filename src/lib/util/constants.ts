40c7b5ceb23db93c7906607663bda5823fb11a96
Gryffon Bellish <owenbellish@gmail.com>
2019-10-20 23:08:01 -0400

    constants: move options to constants file

--- src/lib/util/constants.ts
+++ src/lib/util/constants.ts
@@ -1,7 +1,10 @@
 import { APIWebhookData } from '../types/Interfaces';
 // This is just in case the process.env isn't filled yet.
 import { config } from 'dotenv';
-import { KlasaClientOptions } from 'klasa';
+import { KlasaClientOptions, KlasaClient } from 'klasa';
+import { ServerOptions } from 'https';
+import { ApiRequest } from '../structures/api/ApiRequest';
+import { ApiResponse } from '../structures/api/ApiResponse';
 config();
 
 export const ERROR_WEBHOOK_DATA: APIWebhookData = {
@@ -24,6 +27,51 @@ export const STATS_WEBHOOK_DATA: APIWebhookData = {
 
 export const CLIENT_SECRET = process.env.CLIENT_SECRET || '';
 
+const { PREFIX: prefix, CLIENT_SECRET: clientSecret, CLIENT_ID: clientID, DEFAULT_PROVIDER: defaultProvider } = process.env;
+
+const serverOptions: ServerOptions = {
+	IncomingMessage: ApiRequest,
+	ServerResponse: ApiResponse
+};
+
 export const STARLIGHT_OPTIONS: KlasaClientOptions = {
-	cdnSweepInterval: 60
+	prefix,
+	consoleEvents: {
+		debug: true,
+		log: true,
+		error: true,
+		verbose: true,
+		warn: true
+	},
+	commandEditing: true,
+	commandLogging: true,
+	fetchAllMembers: true,
+	schedule: {
+		interval: 'INTERVAL' in process.env ? Number(process.env.INTERVAL) || 5000 : 5000
+	},
+	readyMessage: (client: KlasaClient): string => `[INTERNAL] Successfully initialized. Ready to serve ${client.guilds.size} guild${client.guilds.size === 1 ? '' : 's'}.`,
+	restRequestTimeout: 60000,
+	disabledEvents: [
+		'TYPING_START',
+		'PRESENCE_UPDATE'
+	],
+	providers: {
+		'default': defaultProvider || 'toml'
+	},
+	console: {
+		colors: {
+			verbose: {
+				time: {
+					background: 'green',
+					text: 'white'
+				}
+			}
+		}
+	},
+	dashboardHooks: {
+		apiPrefix: '/',
+		serverOptions
+	},
+	clientSecret,
+	clientID
 };
