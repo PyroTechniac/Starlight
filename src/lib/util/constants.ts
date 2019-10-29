0c099078335f7fd5d80fcb8475ac708feee6c33e
Gryffon Bellish <owenbellish@gmail.com>
2019-10-28 21:04:59 -0400

    src: remove old things

--- src/lib/util/constants.ts
+++ src/lib/util/constants.ts
@@ -1,29 +1,10 @@
-import { APIWebhookData } from '../types/Interfaces';
 // This is just in case the process.env isn't filled yet.
 import { config } from 'dotenv';
-import { KlasaClientOptions, KlasaClient } from 'klasa';
 import { ServerOptions } from 'https';
+import { KlasaClient, KlasaClientOptions } from 'klasa';
 import { ApiRequest } from '../structures/api/ApiRequest';
 import { ApiResponse } from '../structures/api/ApiResponse';
 config();
-
-export const ERROR_WEBHOOK_DATA: APIWebhookData = {
-	id: '625154971918139412',
-	guild_id: '449679505519411215',
-	channel_id: '625154324992884737',
-	avatar: '394c0e13926fe83edd2bb79b41c6b627',
-	token: process.env.ERROR_WEBHOOK_TOKEN || '',
-	name: 'Starlight Error'
-};
-
-export const STATS_WEBHOOK_DATA: APIWebhookData = {
-	id: '625451204255678464',
-	guild_id: '449679505519411215',
-	channel_id: '625154324992884737',
-	avatar: '394c0e13926fe83edd2bb79b41c6b627',
-	token: process.env.STATS_WEBHOOK_TOKEN || '',
-	name: 'Starlight Stats'
-};
 
 export const CLIENT_SECRET = process.env.CLIENT_SECRET || '';
 
