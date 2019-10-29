029ea0b247cbd8752000ec44a19e8e5fc32e6424
Gryffon Bellish <owenbellish@gmail.com>
2019-10-28 22:52:12 -0400

    lint

--- src/commands/System/Admin/reboot.ts
+++ src/commands/System/Admin/reboot.ts
@@ -3,19 +3,21 @@ import { ApplyOptions } from '../../../lib/util/Decorators';
 import { Events } from '../../../lib/types/Enums';
 
 @ApplyOptions<CommandOptions>({
-    description: (lang): string => lang.get('COMMAND_REBOOT_DESCRIPTION'),
-    guarded: true,
-    permissionLevel: 10
+	description: (lang): string => lang.get('COMMAND_REBOOT_DESCRIPTION'),
+	guarded: true,
+	permissionLevel: 10
 })
 export default class extends Command {
-    public async run(message: KlasaMessage): Promise<never> {
-        await message.sendLocale('COMMAND_REBOOT').catch((err): boolean => this.client.emit(Events.Wtf, err));
 
-        try {
-            await Promise.all(this.client.providers.map((provider): Promise<unknown> => provider.shutdown()));
-            this.client.destroy()
-        } catch { }
+	public async run(message: KlasaMessage): Promise<never> {
+		await message.sendLocale('COMMAND_REBOOT').catch((err): boolean => this.client.emit(Events.Wtf, err));
 
-        return process.exit();
-    }
+		try {
+			await Promise.all(this.client.providers.map((provider): Promise<unknown> => provider.shutdown()));
+			this.client.destroy();
+		} catch { }
+
+		return process.exit();
+	}
+
 }
