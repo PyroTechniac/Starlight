3c5a7e3b08c36693f83d2b2386ad70c4ec28a37c
Gryffon Bellish <owenbellish@gmail.com>
2019-11-08 12:33:58 -0500

    src: add aliases

--- src/serializers/member.ts
+++ src/serializers/member.ts
@@ -1,7 +1,11 @@
-import { Serializer, KlasaGuild, SchemaEntry, Language } from 'klasa';
+import {Serializer, KlasaGuild, SchemaEntry, Language, SerializerOptions} from 'klasa';
 import { GuildMember } from 'discord.js';
 import { noop, toss } from '../lib/util/Utils';
+import {ApplyOptions} from "../lib/util/Decorators";
 
+@ApplyOptions<SerializerOptions>({
+	aliases: ['guildmember']
+})
 export default class extends Serializer {
 
 	public async deserialize(data: any, entry: SchemaEntry, language: Language, guild: KlasaGuild): Promise<GuildMember> {
