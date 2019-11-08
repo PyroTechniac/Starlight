6ad3c2aafe09847305ba718b386af716f8c645b6
Gryffon Bellish <owenbellish@gmail.com>
2019-11-08 12:50:44 -0500

    fix typo

--- src/serializers/member.ts
+++ src/serializers/member.ts
@@ -12,7 +12,7 @@ export default class extends Serializer {
 		if (!guild) throw language.get('RESOLVER_INVALID_GUILD', entry.key);
 		if (data instanceof GuildMember) return data;
 		const member = await this.client.resolvers.run<GuildMember>('member', data, language, guild.members).catch(noop);
-		return member || toss(language.get('RESOVLER_INVALID_MEMBER', entry.key));
+		return member || toss(language.get('RESOLVER_INVALID_MEMBER', entry.key));
 	}
 
 	public serialize(data: GuildMember): string {
