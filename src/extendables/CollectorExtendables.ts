6bfc448da78ec6d4fccb3c170510b295f5c6c520
Gryffon Bellish <owenbellish@gmail.com>
2019-11-26 08:33:58 -0500

    lint

--- src/extendables/CollectorExtendables.ts
+++ src/extendables/CollectorExtendables.ts
@@ -1,14 +1,16 @@
-import {Extendable, ExtendableStore} from 'klasa';
-import {Collector} from 'discord.js'
+import { Extendable, ExtendableStore } from 'klasa';
+import { Collector } from 'discord.js';
 
 
 // This should not use the `ApplyOptions` decorator as TypeScript decorators break Extendables
 export default class CollectorExtendable<K, V> extends Extendable {
-    public constructor(store: ExtendableStore, file: string[], directory: string) {
-        super(store, file, directory, {appliesTo: [Collector]})
-    }
 
-    public *[Symbol.iterator](this: Collector<K, V>): IterableIterator<V> {
-        yield* this.collected.values();
-    }
+	public constructor(store: ExtendableStore, file: string[], directory: string) {
+		super(store, file, directory, { appliesTo: [Collector] });
+	}
+
+	public *[Symbol.iterator](this: Collector<K, V>): IterableIterator<V> {
+		yield *this.collected.values();
+	}
+
 }
