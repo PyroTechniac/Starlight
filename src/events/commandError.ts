18bf1e680be0b95b033be742f91c6d61ca3110ae
Gryffon Bellish <owenbellish@gmail.com>
2019-12-01 20:22:01 -0500

    remove starlight error and just go back to the old method

--- src/events/commandError.ts
+++ src/events/commandError.ts
@@ -1,16 +1,24 @@
 import { Command, Event, KlasaMessage } from 'klasa';
-import { ErrorLike, StarlightError } from '../lib/structures/StarlightError';
-import { Events } from '../lib/types/Enums';
+import { Events, PermissionLevels } from '../lib/types/Enums';
 
 export default class extends Event {
 
-	public async run(message: KlasaMessage, command: Command, params: any[], error: ErrorLike | StarlightError | string): Promise<void> {
-		if (error instanceof Error) this.client.emit(Events.Wtf, `[COMMAND] ${command.path}\n${error.stack || error}`);
-		const resolved = StarlightError.resolve(error);
-		const starlightError = new StarlightError(resolved.message, resolved);
+	public async run(message: KlasaMessage, command: Command, params: unknown[], error: unknown): Promise<void> {
+		if (error instanceof Error) {
+			await this.handleError(message, command, error);
+		} else {
+			await message.send(error).catch((err): boolean => this.client.emit(Events.Wtf, err));
+		}
+	}
 
-		const debug = 'debug' in message.flagArgs;
-		await (debug ? message.sendCode.bind(message, 'JSON') : message.sendMessage.bind(message))(starlightError.message(debug)).catch((err): boolean => this.client.emit(Events.Wtf, err));
+	private async handleError(message: KlasaMessage, command: Command, error: Error): Promise<void> {
+		this.client.emit(Events.Wtf, `[COMMAND] ${command.path}\n${error.stack || error}`);
+		const isOwner = await message.hasAtLeastPermissionLevel(PermissionLevels.BotOwner);
+		if (!isOwner || !error.stack) {
+			await message.sendCode('JSON', error.message).catch((err): boolean => this.client.emit(Events.Wtf, err));
+		} else {
+			await message.sendCode('JSON', error.stack, { split: true }).catch((err): boolean => this.client.emit(Events.Wtf, err));
+		}
 	}
 
 }
