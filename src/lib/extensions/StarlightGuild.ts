5b821ac790f53297aad115d34a512cd522d8c494
Gryffon Bellish <owenbellish@gmail.com>
2019-12-04 20:47:49 -0500

    src: add guild#fetchBan

--- src/lib/extensions/StarlightGuild.ts
+++ src/lib/extensions/StarlightGuild.ts
@@ -1,9 +1,22 @@
-import { Guild, Structures } from 'discord.js';
+import { Guild, Snowflake, Structures, User } from 'discord.js';
 import { MemberNicknames } from '../util/cache/MemberNicknames';
+import { api } from '../util/Api';
+import { APIUserData } from '../types/Interfaces';
 
 export class StarlightGuild extends Structures.get('Guild') {
 
 	public readonly nicknames = new MemberNicknames(this);
+
+	public fetchBan(id: Snowflake): Promise<{ user: User; reason: string }> {
+		return (api(this.client)
+			.guilds(this.id)
+			.bans(id)
+			.get() as Promise<{user: APIUserData; reason: string}>)
+			.then((data): { user: User; reason: string } => ({
+				user: this.client.users.add(data.user),
+				reason: data.reason
+			}));
+	}
 
 }
 
