02381506676aab50f479786f9b08658cbe4eb251
Gryffon Bellish <owenbellish@gmail.com>
2019-12-09 10:54:32 -0500

    src: fetch avatars

---
+++ src/lib/extensions/StarlightUser.ts
@@ -0,0 +1,17 @@
+import {Structures, ImageSize, User} from 'discord.js';
+
+export class StarlightUser extends Structures.get('User') {
+    public async fetchAvatar(size: ImageSize = 512): Promise<Buffer> {
+        const url = this.displayAvatarURL({format: 'png', size});
+        try {
+            return await this.client.cdn
+                .url(url)
+                .type('Buffer')
+                .get<Buffer>();
+        } catch (e) {
+            throw `Could not download the profile avatar: ${e}`;
+        }
+    }
+}
+
+Structures.extend('User', (): typeof User => StarlightUser);
