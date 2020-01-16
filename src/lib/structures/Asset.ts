db36338d8ef0c301804a02b9bf708f5899001e33
Gryffon Bellish <owenbellish@gmail.com>
2020-01-16 10:46:16 -0500

    add a more descriptive error

--- src/lib/structures/Asset.ts
+++ src/lib/structures/Asset.ts
@@ -1,23 +1,28 @@
 import { Piece, PieceJSON, PieceOptions } from 'klasa';
 import { readFile } from 'fs-nextra';
 import { MessageAttachment } from 'discord.js';
-import { assetsFolder } from '../util/Utils';
+import { assetsFolder, noop } from '../util/Utils';
 
 export abstract class Asset extends Piece {
 
 	private raw: Buffer | null = null;
+
+	private initialized: boolean = false;
 
 	public abstract get filepath(): string;
 
 	public abstract get filename(): string;
 
 	public get attachment(): MessageAttachment {
-		if (this.raw === null) throw new Error('Cannot access file when asset isn\'t loaded');
+		if (this.raw === null) {
+			throw new Error(this.initialized ? 'Failed to load asset.' : 'Asset has not been loaded.');
+		}
 		return new MessageAttachment(this.raw, this.filename);
 	}
 
 	public async init(): Promise<void> {
-		this.raw = await readFile(this.filepath);
+		this.initialized = true;
+		this.raw = await readFile(this.filepath).catch(noop);
 	}
 
 	public toJSON(): PieceAssetJSON {
